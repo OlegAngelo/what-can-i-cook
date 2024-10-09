0c3792499ffb7a1d0c7900f69ad52ab964c6adca
OlegAngelo <naubalagtas@gmail.com>
2024-10-09 21:41:09 +0800

    Refactor folder and files for routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  async redirects() {
+    return [
+      {
+        source: '/landingpage/landing-page',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/ingredients',
+        destination: '/ingredients/list',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
