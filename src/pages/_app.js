d47eea887090df31dd670c4ed1f5b271c00330ff
OlegAngelo <naubalagtas@gmail.com>
2024-10-14 20:08:45 +0800

    optimize seo, accessibility, and best practices to green

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>What Can I Cook?</title>
+        <meta name="description" content="Free Web tutorials"></meta>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  )
 }
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -67,6 +67,7 @@ module.exports = {
         		xl: '1280px',
         		'2xl': '1536px',
       		},
+			purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   		}
   	},
   	plugins: [require("tailwindcss-animate")],
