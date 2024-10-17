a2b7338d1a2dc60a1a695dffb410d29fd2bad9ff
Nino Angelo Balagtas <naubalagtas@gmail.com>
2024-10-17 15:37:27 +0800

    Change api to remote vercel domain (#23)

--- src/pages/api/WhatCanICookAPI.js
+++ src/pages/api/WhatCanICookAPI.js
@@ -1,38 +1,42 @@
 import axios from "axios";
 
-const localApi = "http://localhost:4000"
+const localApi = "http://localhost:4000";
+const remoteApi = "what-can-i-cook-api.vercel.app";
 
 const WhatCanICookAPI = {
-  getRecipeList: async (ingredients) => {
-    if (ingredients) {
-      return axios
-      .get(`${localApi}/`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        params: {
-          ingredients: ingredients,
-        }
-      })
-      .then((response) => {
-        // Check for a message indicating no recipes found
-        if (response.data.message) {
-          return { message: response.data.message }; // Return the message if no recipes found
-        }
+	getRecipeList: async (ingredients) => {
+		if (ingredients) {
+			return axios
+				.get(`${remoteApi}/`, {
+					headers: {
+						"Content-Type": "application/json",
+					},
+					params: {
+						ingredients: ingredients,
+					},
+				})
+				.then((response) => {
+					// Check for a message indicating no recipes found
+					if (response.data.message) {
+						return { message: response.data.message }; // Return the message if no recipes found
+					}
 
-        // If recipes are found, return the data
-        return response.data; 
-      })
-      .catch((error) => {
-        // Handle errors from the backend
-        if (error.response && process.env.NODE_ENV !== 'production') {
-          return { error: error.response.data.error };
-        } else {
-          return { error: 'Something went wrong. Please check your connection or try again later.' };
-        }
-      });
-    }
-  },
-}
+					// If recipes are found, return the data
+					return response.data;
+				})
+				.catch((error) => {
+					// Handle errors from the backend
+					if (error.response && process.env.NODE_ENV !== "production") {
+						return { error: error.response.data.error };
+					} else {
+						return {
+							error:
+								"Something went wrong. Please check your connection or try again later.",
+						};
+					}
+				});
+		}
+	},
+};
 
 export default WhatCanICookAPI;
