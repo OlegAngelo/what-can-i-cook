66c7aa19fd2f7957b4fb47dbb522fd5eef3f1496
OlegAngelo <naubalagtas@gmail.com>
2024-10-25 02:33:20 +0800

    Make few adjustments for demo day

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,77 @@
-import React, { Suspense, useState, useEffect } from 'react';
+import React, { Suspense, useState, useEffect } from "react";
 // import Cookies from 'js-cookie';
 
-const HandleIngredientInputComponent = React.lazy(() => import('./function/HandleIngredient.js'));
-const LazyRecipesList = React.lazy(() => import('./recipes/list.js'));
-const Modal = React.lazy(() => import('./recipes/detail.js'));
+const HandleIngredientInputComponent = React.lazy(() =>
+	import("./function/HandleIngredient.js")
+);
+const LazyRecipesList = React.lazy(() => import("./recipes/list.js"));
+const Modal = React.lazy(() => import("./recipes/detail.js"));
 
 const Home = () => {
-  const [ingredientValue, setIngredientValue] = useState("");
-  const [recipes, setRecipes] = useState(null);
-  const [message, setMessage] = useState("");
+	const [ingredientValue, setIngredientValue] = useState("");
+	const [recipes, setRecipes] = useState(null);
+	const [message, setMessage] = useState("");
 
-  // Modal states
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedRecipe, setSelectedRecipe] = useState(null);
+	// Modal states
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [selectedRecipe, setSelectedRecipe] = useState(null);
 
-  useEffect(() => {
-    const savedIngredient = localStorage.getItem("ingredients");
-    if (savedIngredient) {
-      setIngredientValue(savedIngredient);
-    }
-  }, []);
-  // Function to open the modal
-  const onRecipeClick = (recipe) => {
-    setSelectedRecipe(recipe);
-    setIsModalOpen(true);
-  };
+	useEffect(() => {
+		const savedIngredient = localStorage.getItem("ingredients");
+		if (savedIngredient) {
+			setIngredientValue(savedIngredient);
+		}
+	}, []);
+	// Function to open the modal
+	const onRecipeClick = (recipe) => {
+		setSelectedRecipe(recipe);
+		setIsModalOpen(true);
+	};
 
-  // Function to close the modal
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedRecipe(null); 
-  };
-  return (
-    <div
-      className={`font-geist font-geistMono grid
+	// Function to close the modal
+	const closeModal = () => {
+		setIsModalOpen(false);
+		setSelectedRecipe(null);
+	};
+	return (
+		<div
+			className={`font-geist font-geistMono grid
         grid-auto-rows-auto
         justify-items-center min-h-screen p-4 pb-20 max-w-full overflow-hidden font-geist`}
-    >
-      <header className={`flex flex-col row-start-1 items-center sm:items-start 
-          ${recipes ? 'self-start' : 'self-center'}`}
-      >
-        <div className="text-xl text-center font-geistMono mb-2">
-          I want to cook but my ingredients are only{" "}
-            <Suspense fallback={<div>Loading...</div>}>
-              <HandleIngredientInputComponent
-                ingredientValue={ingredientValue}
-                setIngredientValue={setIngredientValue}
-                setMessage={setMessage}
-                setRecipes={setRecipes}
-              />
-            </Suspense>
-        </div>
-      </header>
+		>
+			<header
+				className={`flex flex-col row-start-1 items-center sm:items-start 
+          ${recipes ? "self-start" : "self-center"}`}
+			>
+				<div className="text-xl text-center font-geistMono mb-2">
+					I want to cook but my ingredients are only{" "}
+					<Suspense fallback={<div></div>}>
+						<HandleIngredientInputComponent
+							ingredientValue={ingredientValue}
+							setIngredientValue={setIngredientValue}
+							setMessage={setMessage}
+							setRecipes={setRecipes}
+						/>
+					</Suspense>
+				</div>
+			</header>
 
-      {message && (
-        <div className="row-start-2 text-center text-red-500">
-          {message}
-        </div>
-      )}
+			{message && (
+				<div className="row-start-2 text-center text-red-500">{message}</div>
+			)}
 
-      {recipes && (
-        <main className="row-start-2 gap-6 items-center justify-center w-full">
-          <Suspense fallback={<div>Loading...</div>}>
-            <LazyRecipesList recipes={recipes} onRecipeClick={onRecipeClick} />
-          </Suspense>
-        </main>
-      )}
+			{recipes && (
+				<main className="row-start-2 gap-6 items-center justify-center w-full">
+					<Suspense fallback={<div></div>}>
+						<LazyRecipesList recipes={recipes} onRecipeClick={onRecipeClick} />
+					</Suspense>
+				</main>
+			)}
 
-      {/* Modal component */}
-      <Modal isOpen={isModalOpen} onClose={closeModal} recipe={selectedRecipe} />
-    </div>
-  );
-}
+			{/* Modal component */}
+			<Modal isOpen={isModalOpen} onClose={closeModal} recipe={selectedRecipe} />
+		</div>
+	);
+};
 
 export default Home;
--- src/pages/recipes/list-item.js
+++ src/pages/recipes/list-item.js
@@ -1,45 +1,43 @@
-import { ClockIcon, BellIcon } from "@heroicons/react/24/outline"
-import Image from "next/image"
+import { ClockIcon, BellIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 const RecipesListItem = ({ recipe }) => {
-    if (!recipe) return <div></div>
+	if (!recipe) return <div></div>;
 
-    return (
-        <div className="flex flex-col sm:flex-row border border-[-var(--foreground)] rounded mb-5">
-            <div className="flex-none w-[140px]">
-                <Image
-                    src={recipe.image}
-                    alt={recipe.name}
-                    width={100}
-                    height={500}
-                    quality={100}
-                    style={{
-                        maxWidth: '140px',
-                        minWidth: '140px',
-                        width: '100%',
-                        height: 'auto',
-                    }}
-                    loading="lazy"
-                    className="rounded m-2 rounded"
-                />
-            </div>
-            <div className="flex-grow flex flex-col m-4">
-                <p className="flex-1 text-left xs:text-sm sm:text-base max-h-[6rem] overflow-hidden font-bold text-[-var(--foreground)] font-geist">
-                    {recipe.name}
-                </p>
-                <div className="flex-2 flex-col justify-evenly overflow-hidden">
-                    <div className="flex items-center text-sm text-[-var(--foreground)">
-                        <ClockIcon className="w-5 h-5 mr-2" />
-                        <span className="italic">{recipe.total_time} mins</span>
-                    </div>
-                    <div className="flex items-center text-sm text-[-var(--foreground)]">
-                        <BellIcon className="w-5 h-5 mr-2" />
-                        <span className="italic">{recipe.meal_type}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="flex flex-col sm:flex-row border border-[var(--foreground)] rounded mb-5 items-center">
+			<Image
+				src={recipe.image}
+				alt={recipe.name}
+				width={100}
+				height={500}
+				quality={100}
+				style={{
+					maxWidth: "140px",
+					minWidth: "140px",
+					width: "100%",
+					height: "auto",
+				}}
+				loading="lazy"
+				className="rounded m-2 items-center justify-center text-center"
+			/>
+			<div className="flex-grow flex flex-col m-4 gap-y-3">
+				<p className="flex-1 text-left xs:text-sm sm:text-base max-h-[6rem] overflow-hidden font-bold text-[-var(--foreground)] font-geist">
+					{recipe.name}
+				</p>
+				<div className="flex-2 flex-col justify-evenly overflow-hidden">
+					<div className="flex items-center text-sm text-[-var(--foreground)">
+						<ClockIcon className="w-5 h-5 mr-2" />
+						<span className="italic">{recipe.total_time} mins</span>
+					</div>
+					<div className="flex items-center text-sm text-[var(--foreground)]">
+						<BellIcon className="w-5 h-5 mr-2" />
+						<span className="italic">{recipe.meal_type}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default RecipesListItem;
